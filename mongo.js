93b822fd0819962506e8e7e4215f2675797e9a2e
Oh Byeongmin <byeongmin.oh@aalto.fi>
2024-07-19 14:35:32 +0900

    3.12 implemented

--- mongo.js
+++ mongo.js
@@ -1,32 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://bmaron:rhdiddl789@fullstack-cluster.uol77bl.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fullstack-cluster`
+const url = `mongodb+srv://bmaron:${password}@fullstack-cluster.uol77bl.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fullstack-cluster`;
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false);
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+});
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema);
 
-const person = new Person({
-  name: 'Test Naming',
-  number: '010-44000',
-})
+if (process.argv.length === 3) {
+  // Display all entries in the phonebook
+  Person.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log('Invalid number of arguments. Provide password, name, and number to add an entry.');
+  mongoose.connection.close();
+}
