d4e0fad18c187a047279695a9c9454d2c05b4b28
Oh Byeongmin <byeongmin.oh@aalto.fi>
2024-07-20 14:54:53 +0900

    number validate testing

--- models/phone.js
+++ models/phone.js
@@ -13,6 +13,20 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
+const phoneNumberValidator = (value) => {
+  const parts = value.split('-');
+  if (parts.length !== 2) {
+    return false;
+  }
+
+  const [part1, part2] = parts;
+  if (!/^\d{2,3}$/.test(part1) || !/^\d+$/.test(part2)) {
+    return false;
+  }
+
+  return true;
+}
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -23,9 +37,8 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 8,
     validate: {
-      validator: function(v) {
-        return /\d{2,3}-\d{+$}/.test(v);
-      }
+      validator: phoneNumberValidator,
+      message: props => `${props.value} is not a valid phone number` 
     },
     required: true
   },
