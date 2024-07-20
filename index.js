f23302fee84a50f9df5a5307378edbdf324dacea
Oh Byeongmin <byeongmin.oh@aalto.fi>
2024-07-20 16:05:54 +0900

    3.22 ESLint added, all error solved

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
 require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
 const Person = require('./models/phone')
-
-let persons = [];
 
 app.use(express.static('dist'))
 
 // Middleware for logging requests
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 // Define a custom token to log POST request data
 morgan.token('postData', (req) => {
   if (req.method === 'POST') {
-    return JSON.stringify(req.body);
+    return JSON.stringify(req.body)
   }
-  return '';
-});
+  return ''
+})
 
 // Use Morgan middleware with custom format
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
-app.use(express.json());
-app.use(requestLogger);
+app.use(express.json())
+app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+  response.send('<h1>Hello World!</h1>')
+})
 
 app.get('/info', (request, response) => {
-  const currentTime = new Date();
-  const numOfPeople = Person.length;
+  const currentTime = new Date()
+  const numOfPeople = Person.length
 
   const responseText = `
     <p>Phonebook has info for ${numOfPeople} people</p>
     <p>${currentTime}</p>
-  `;
+  `
 
-  response.send(responseText);
-});
+  response.send(responseText)
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
-    response.json(people);
+    response.json(people)
   })
-});
+})
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number (possibly both) is missing' 
-    });
-  } 
+    return response.status(400).json({
+      error: 'name or number (possibly both) is missing'
+    })
+  }
 
   const person = new Person({
     name: body.name,
@@ -73,39 +71,37 @@ app.post('/api/persons', (request, response, next) => {
   })
 
   person.save().then(savedPerson => {
-    console.log(`new person ${savedPerson.name} with ${savedPerson.number} added`);
+    console.log(`new person ${savedPerson.name} with ${savedPerson.number} added`)
     response.json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 
-});
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)  
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
-});
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
+    .then(response.status(204).end())
     .catch(error => next(error))
-});
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const {name, number} = request.body
+  const { name, number } = request.body
 
   Person.findByIdAndUpdate(
-    request.params.id, 
-    {name, number}, 
+    request.params.id,
+    { name, number },
     { new: true, runValidators: true, context: 'query' }
   )
     .then(updatedPerson => {
@@ -114,7 +110,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
@@ -132,5 +128,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
