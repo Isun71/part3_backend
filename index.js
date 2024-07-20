f5177d62f31342e514249eec0fc23d47e4daa842
Oh Byeongmin <byeongmin.oh@aalto.fi>
2024-07-20 19:04:44 +0900

    restructure for future parts

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
+// will be trimmed to:
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+// -----------------------
+
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-const Person = require('./models/phone')
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
-
-// Middleware for logging requests
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
 
 // Define a custom token to log POST request data
 morgan.token('postData', (req) => {
@@ -30,31 +30,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.use(express.json())
 app.use(requestLogger)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/info', (request, response) => {
-  const currentTime = new Date()
-  const numOfPeople = Person.length
-
-  const responseText = `
-    <p>Phonebook has info for ${numOfPeople} people</p>
-    <p>${currentTime}</p>
-  `
-
-  response.send(responseText)
-})
-
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(people => {
-    response.json(people)
-  })
-})
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
@@ -78,23 +53,6 @@ app.post('/api/persons', (request, response, next) => {
 
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndDelete(request.params.id)
-    .then(response.status(204).end())
-    .catch(error => next(error))
-})
 
 app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body
@@ -111,18 +69,6 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
 
 app.use(errorHandler)
 
