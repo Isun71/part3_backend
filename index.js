225611efd76ca7b88c8a5946643b133fc09c0558
Oh Byeongmin <byeongmin.oh@aalto.fi>
2024-07-20 12:35:27 +0900

    3.19 ongoing

--- index.js
+++ index.js
@@ -61,10 +61,6 @@ app.get('/api/persons', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body;
 
-  // if (body.content === undefined) {
-  //   return response.status(400).json({ error: 'content missing'})
-  // }
-
   if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'name or number (possibly both) is missing' 
@@ -83,13 +79,11 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  // Old version for posting:
-  // persons = persons.concat(person);
-  // response.json(person);
   person.save().then(savedPerson => {
     console.log(`new person ${savedPerson.name} with ${savedPerson.number} added`);
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 
 });
 
@@ -126,14 +120,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const {name, number} = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    {name, number}, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -147,7 +140,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
