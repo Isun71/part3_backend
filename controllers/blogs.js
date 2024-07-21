66b1aa91fe0fd7e481892a88a959b0d138951dea
Oh Byeongmin <byeongmin.oh@aalto.fi>
2024-07-22 01:41:49 +0900

    add console log to catch error

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,74 +1,64 @@
+// controllers/blogs.js
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({}).populate('blogs', { username: 1, name: 1 })
-
-  response.json(blogs)
-})
-
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  if (blog) {
-    response.json(blog)
-  } else {
-    response.status(404).end()
+  try {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'internal server error' })
   }
 })
 
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
+  try {
+    const user = request.user
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
+    if (!user) {
+      return response.status(401).json({ error: 'user missing or invalid' })
+    }
+
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes || 0,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'internal server error' })
   }
-
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.json(updatedBlog)
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const user = request.user
-  if (!user) {
-    return response.status(401).json({ error: 'user missing or invalid' })
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  try {
+    const user = request.user
+    const blog = await Blog.findById(request.params.id)
+
+    if (!blog) {
+      return response.status(404).json({ error: 'blog not found' })
+    }
+
+    if (!user || blog.user.toString() !== user._id.toString()) {
+      return response.status(401).json({ error: 'unauthorized user' })
+    }
+
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'internal server error' })
   }
-
-  const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes || 0,
-    user: user._id
-  })
-
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-
-  response.status(201).json(savedBlog)
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-  const user = request.user
-  const blog = await Blog.findById(request.params.id)
-
-  if (!blog) {
-    return response.status(404).json({ error: 'blog not found' })
-  }
-
-  if (!user || blog.user.toString() !== user._id.toString()) {
-    return response.status(401).json({ error: 'user not found / unauthorized user' })
-  }
-
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
 })
 
 module.exports = blogsRouter
